8e40d362edd9200da101fefd393ff3d812df13a1
Partizan <67509212+cyberpartizan@users.noreply.github.com>
2020-08-15 14:12:58 +0300

    Initial commit

---
+++ AGPU_Schedule_Parser.py
@@ -0,0 +1,149 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import time
+import threading
+
+URL = 'https://it-institut.ru/Raspisanie/SearchedRaspisanie?SearchId=880&SearchString=%D0%92%D0%9C-%D0%98%D0%92%D0%A2-2-1&Type=Group&OwnerId=118&WeekId='
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+
+def get_html(url, headers, params=None):  # HTML страница
+    r = requests.get(url, headers=headers, params=params)
+    return r.text
+
+
+def get_content(html):  # Конвертация в BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+
+def get_day_lessons(hoursandwith, daysandlessons, weekday):  # Вычисления расписания за день
+    weekday = weekday - 1
+    i = 0
+    j = 0
+    lessons = []
+    while j <= len(hoursandwith) - 1:  # Вычисление пустых клеток и обнаружение двух клеток в одной
+        if (hoursandwith[j]['hourWidth'] == '2') and (daysandlessons[weekday]['lessons'][i]['lessonWidth'] == '1'):
+            lessons.append((hoursandwith[j]['hour'] + '\n' + daysandlessons[weekday]['lessons'][i]['lesson'] + '\n' +
+                            daysandlessons[weekday]['lessons'][i + 1]['lesson']).replace('\n\n', '').replace(
+                '\n(ВМ-ИВТ-2-1)', ''))
+            i += 1
+        else:
+            if daysandlessons[weekday]['lessons'][i]['lesson'] != '':
+                lessons.append(
+                    (hoursandwith[j]['hour'] + '\n' + daysandlessons[weekday]['lessons'][i]['lesson']).replace('\n\n','').replace('\n(ВМ-ИВТ-2-1)', ''))
+        i += 1
+        j += 1
+    return {'day': daysandlessons[weekday]['day'], 'dayLessons': lessons}
+
+
+def get_week_lessons(hoursandwith, daysandlessons):  # Вычисление расписания за неделю
+    week = []
+    for i in range(1, 8):
+        week.append(get_day_lessons(hoursandwith, daysandlessons, i))
+    return week
+
+
+def get_week_count_and_weekday(day, month, year):  # Вычисление недели в URL и номера дня недели
+    startweek = 2357
+    startday = datetime.date(2019, 9, 2)
+    endday = datetime.date(year, month, day)
+    plusweek = int((endday - startday).days / 7)
+    weekday = endday.weekday() + 1
+    weekcount = startweek + plusweek
+    return {'weekday': weekday, 'weekcount': weekcount}
+
+
+def split_date_to_numbers(date):  # Разделение даты по дням / месяцам / лет
+    temp = []
+    if type(date) == datetime.date:
+        temp.append(date.day)
+        temp.append(date.month)
+        temp.append(date.year)
+    else:
+        if "." in date:
+            temp = list(map(int, date.split('.')))
+        elif "," in date:
+            temp = list(map(int, date.split(',')))
+        else:
+            print("Неправельный формат даты дата")
+    return temp
+
+
+def get_lesson_by_date(date):  # Расписание по дате пример формата 9.3.2020
+    hoursandwith = []
+    daysandlessons = []
+    datenumbers = split_date_to_numbers(date)
+    weekCount_weekDay = get_week_count_and_weekday(datenumbers[0], datenumbers[1], datenumbers[2])
+    finalURL = URL + str(weekCount_weekDay['weekcount'])
+    html = get_html(finalURL, headers)
+    content = get_content(html)
+    tr = content.findAll('tr')  # Находим все строки таблицы (включая заголовки)
+
+    for th in tr[0].findAll('th')[1:]:  # Заголовки и их ширина
+        hoursandwith.append({'hour': th.find('span').get_text(),
+                             'hourWidth': th['colspan']
+                             })
+
+    for line in tr[1:]:  # Пары и их ширина
+        temp = []
+        for lesson in line.findAll('td'):
+            temp.append({'lesson': lesson.get_text(separator='\n'),
+                         'lessonWidth': lesson['colspan']})
+        daysandlessons.append({'day': line.find('th').get_text(separator='\n'),
+                               'lessons': temp})
+
+    day = (get_day_lessons(hoursandwith, daysandlessons, weekCount_weekDay['weekday']))
+    return day
+
+
+def today(days=0):
+    date = datetime.datetime.now().date() + datetime.timedelta(days=days)
+    res = String_day(get_lesson_by_date(date))
+    return res
+
+
+def bydate(date):
+    day = get_lesson_by_date(date)
+    res = String_day(day)
+    return res
+
+
+def check_today_schedule_change():  # Проверка если расписание изминилось на сегодня
+    while True:
+        currentday = today()
+        while (datetime.datetime.now().hour >= 8) and (datetime.datetime.now().hour < 18):
+            if currentday == today():
+                time.sleep(900)
+            else:
+                currentday = today()
+
+        time.sleep(51000)
+
+
+def start_scan():
+    thread = threading.Thread(target=check_today_schedule_change, daemon=True)
+    thread.start()
+
+
+def String_day(daydict):
+    day = daydict['day'] + '\n\n'
+    lessons = ''
+    for lesson in daydict['dayLessons']:
+        lessons = lessons + lesson + '\n\n'
+    return day + lessons
+
+
+def String_week(weekdict):
+    WeekLessons = ''
+    for daydict in weekdict:
+        WeekLessons = WeekLessons + String_day(daydict) + '\n'
+    return WeekLessons
+
+# day20_4_2020=today(-100)
+# print(String_day(day20_4_2020))
+# start_scan()
+# while True:
+# time.sleep(5)
---
+++ VK_Bot_AGPU_Schedule.py
@@ -0,0 +1,80 @@
+import socket
+import requests
+import urllib3
+import vk_api
+import AGPU_Schedule_Parser as parser
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import time
+
+
+# Импорт API ключа(токена) из отдельного файла
+APIKEYSS = "6d3e7800807862b42f09c5b0adeb32c71d8646aef4e70ec1db414dba89d328d2e48e49486f2fdae46333d"
+print("Бот работает...")
+group_id = '197937466'  # Указываем id сообщества, изменять только здесь!
+oshibka = 0  # обнуление счетчика ошибок
+
+
+def main():
+    global oshibka  # Счетчик ошибок
+    try:
+        vk_session = vk_api.VkApi(token=APIKEYSS)  # Авторизация под именем сообщества
+        longpoll = VkBotLongPoll(vk_session, group_id)
+        vk = vk_session.get_api()
+        try:
+            # Отправка текстового сообщения
+            def String_day(daydict):
+                day = daydict['day'] + '\n'
+                lessons = ''
+                for lesson in daydict['dayLessons']:
+                    lessons = lessons + lesson + '\n'
+                return day + lessons
+
+            def String_week(weekdict):
+                WeekLessons = ''
+                for daydict in weekdict:
+                    WeekLessons = WeekLessons + String_day(daydict) + '\n'
+                return WeekLessons
+
+            def send_msg(ms_g):
+                vk.messages.send(peer_id=event.object.peer_id, random_id=0, message=ms_g)
+
+            for event in longpoll.listen():  # Постоянный листинг сообщений
+                if event.type == VkBotEventType.MESSAGE_NEW:  # Проверка на приход сообщения
+                    # Логика ответов
+                    # Текстовые ответы -----------------------------------------------------------------------------
+                    if event.obj.text == "/сегодня" or event.obj.text == "/с":
+                        send_msg(parser.today())
+                    elif event.obj.text == "/завтра" or event.obj.text == "/з":
+                        send_msg(parser.today(1))
+                    elif event.obj.text == "/послезавтра" or event.obj.text == "/пз":
+                        send_msg(parser.today(2))
+                    elif event.obj.text == "/вчера" or event.obj.text == "/в":
+                        send_msg(parser.today(-1))
+                    elif event.obj.text == "/позавчера" or event.obj.text == "/пв":
+                        send_msg(parser.today(-2))
+                    elif "/дата" in event.obj.text or "/д" in event.obj.text:
+                        split = event.obj.text.split()
+                        date = split[1]
+                        send_msg(parser.bydate(date))
+
+
+        except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError,
+                urllib3.exceptions.NewConnectionError, socket.gaierror):
+            oshibka = oshibka + 1
+            print("Произошла ошибка" + '№' + str(oshibka) + " - ошибка подключения к вк!!! Бот будет перезапущен!!!")
+            time.sleep(5.0)
+            main()
+        finally:
+            oshibka = oshibka + 1
+            print("Произошла ошибка" + '№' + str(oshibka) + "!!! Бот будет перезапущен!!!")
+            main()
+    except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError,
+            urllib3.exceptions.NewConnectionError, socket.gaierror):
+        oshibka = oshibka + 1
+        print("Произошла ошибка" + '№' + str(oshibka) + " - ошибка подключения к вк!!! Бот будет перезапущен!!!")
+        time.sleep(5.0)
+        main()
+
+
+if __name__ == '__main__':
+    main()
